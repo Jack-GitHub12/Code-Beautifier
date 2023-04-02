a6d1a243f79a8549e9314710b539c199b7383c85
Jack <72348727+Jack-GitHub12@users.noreply.github.com>
2023-04-02 16:16:38 -0500

    backend
    added more listeners and error handling to the buttons

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputCode = document.getElementById("inputCode");
     const outputCode = document.getElementById("outputCode");
     const beautifyBtn = document.getElementById("beautifyBtn");
+    const clearBtn = document.getElementById("clearBtn");
+    const pasteBtn = document.getElementById("pasteBtn");
+    const copyBtn = document.getElementById("copyBtn");
 
     themeSwitch.addEventListener("change", () => {
         document.body.classList.toggle("dark-mode");
@@ -14,5 +17,27 @@ document.addEventListener("DOMContentLoaded", () => {
         const beautifiedCode = js_beautify(code);
         outputCode.textContent = beautifiedCode;
         hljs.highlightBlock(outputCode);
+    });
+
+    clearBtn.addEventListener("click", () => {
+        inputCode.value = "";
+        outputCode.textContent = "";
+    });
+
+    pasteBtn.addEventListener("click", async () => {
+        try {
+            const text = await navigator.clipboard.readText();
+            inputCode.value = text;
+        } catch (err) {
+            console.error("Failed to read clipboard contents: ", err);
+        }
+    });
+
+    copyBtn.addEventListener("click", async () => {
+        try {
+            await navigator.clipboard.writeText(outputCode.textContent);
+        } catch (err) {
+            console.error("Failed to copy text: ", err);
+        }
     });
 });
